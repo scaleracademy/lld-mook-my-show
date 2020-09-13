3605f7e4716df8717f30377d2ccbec9927b11c8f
Pragy Agarwal <agar.pragy@gmail.com>
2020-09-13 15:03:03 +0530

    init

---
+++ src/main/java/com/bookmyshow/core/CoreApplication.java
@@ -0,0 +1,13 @@
+package com.bookmyshow.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookmyshow/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmyshow.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
